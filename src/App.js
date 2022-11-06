bd312d8aa7905910f93479672af11d0f78e9fa29
Ali Shaib <ali@gtlogic.com>
2022-11-06 20:20:13 +0200

    done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HomeScreen } from "./screens/home";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import StoreListing from "./components/store-listing";
 
-function PageHeader() {
+function Root() {
   return (
     <div className="container">
       <Navbar />
@@ -16,9 +16,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageHeader />}>
+        <Route path="/" element={<Root />}>
           <Route index element={<HomeScreen />} />
-          <Route path="/store/:storeId" element={<StoreListing />} />
+          <Route path="/stores">
+            <Route path="/:storeId" element={<StoreListing />}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
